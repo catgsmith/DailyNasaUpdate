4b691a318fd08eb9f061e0e3afb1c585284bcadc
Catherine Gaughan-Smith <catgsmith@hotmail.com>
2016-11-24 10:41:31 +0000

    change the url for ISS flyby feed

--- NasaDaily/www/NASArough.js
+++ NasaDaily/www/NASArough.js
@@ -1,61 +1,35 @@
-console.log("~Log: hello ");
-btnGo.addEventListener("click", getLocation)
-let xhr = new XMLHttpRequest()
+var btnGo = document.getElementById("btnGo");
 
-function getLocation(){
+
+btnGo.addEventListener("click", getLocation);
+var xhr = new XMLHttpRequest();
+
+function getLocation() {
+
     console.log("clicked button: ");
-    let latitude = document.getElementById("whichLat").value
-    let longitude = document.getElementById("whichLon").value 
-    
-    let whichData = `http://api.open-notify.org/iss-pass.json?q=${latitude},${longitude}`
-    
-    let whichMethod = "GET"
-    xhr.open(whichMethod, whichData)
-    
-    xhr.onreadystatechange = handleReadyState
-    xhr.send()
-    
+    var latitude = document.getElementById("whichLat").value;
+    var longitude = document.getElementById("whichLon").value;
+
+    //http://api.open-notify.org/iss-pass.json?lat=53&lon=9
+    var whichData = "http://api.open-notify.org/iss-pass.json?lat=" + latitude +
+        "&lon=" + longitude + ""; // add the api key here!
+
+    var whichMethod = "GET";
+    xhr.open(whichMethod, whichData);
+
+    xhr.onreadystatechange = handleReadyState;
+    xhr.send();
+
 }
-    
-    function handleReadyState(response){
-        if (xhr.readyState == 4 && xhr.status == 200){
-         let data = JSON.parse(xhr.response)   
-            console.log(data.latitude)
-            
-            let desc=data.latitude[0].description
-            document.getElementById("output").innerHTML=desc
-            
-            
-        }
-        
+
+function handleReadyState(response) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var data = JSON.parse(xhr.response);
+        console.log(data);
+
+        // var desc = data.latitude[0].description;
+        // document.getElementById("output").innerHTML = desc;
+    } else {
+        console.log("oops!");
     }
-    
-    
-                           
-    
-
-
-
-                                      
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                      
-                                      
-       
+}
